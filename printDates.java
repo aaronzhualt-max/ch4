6a5a989cd7ac1e4f225bbfd8953703810740598e
aaronzhualt-max <aaronzhualt@gmail.com>
2026-09-23 17:07:39 -0700

    Add files via upload

---
+++ printDates.java
@@ -0,0 +1,13 @@
+public class printDates {
+	public static void printAmerican(String day, int date, String month, int year) {
+		System.out.println(day + ", " + month + " " + date + ", " + year);
+	}
+	public static void printEuropean(String day, int date, String month, int year) {
+		System.out.println(day + " " + date + " " + month + " " + year);
+	}
+	public static void main(String[] args) {
+	printAmerican("Tuesday", 22, "September", 2026);
+	printEuropean("Tuesday", 22, "September", 2026);
+	}
+}
+
