6c2ce196bef2e344a6367113c69d50e48819631a
aaronzhualt-max <aaronzhualt@gmail.com>
2026-09-24 13:33:54 -0700

    Add files via upload

---
+++ Multadd.java
@@ -0,0 +1,9 @@
+public class Multadd {
+	public static double Multadd (double a, double b, double c) {
+		return a*b+c;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(Multadd(1, 2, 3));
+	}
+}
